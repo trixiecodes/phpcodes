c097b86fc8d9dbf986fc1ddf61f99d6696b0a5c2
trixiecodes <63406226+trixiecodes@users.noreply.github.com>
2020-04-10 23:37:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-include('db.php'); //we include the database file so we can call out the variable $conn.
 
-$stmt = $conn -> prepare("SELECT * FROM user WHERE id = ?"); //this is like an empty placeholder where it tells the sql that we will use this later on
+//include the database file so we can call out the variable $conn
+include('db.php');
 
-$id = 3;
-$stmt -> bind_param('i', $id); //the first parameter means integer, so if it's string, it should be 's', the 2nd parameter is the variable which holds the value of id in our table 'user'.
+$stmt = $conn -> prepare("SELECT * FROM users WHERE id = ?"); //this is like an empty placeholder where it tells the sql that we will use this later on
+
+$id = 2; //example, this should echo the data which has the id of 2
+$stmt -> bind_param('i',$id); //the first parameter means integer, so if it's string, it should be 's', the 2nd parameter is the variable which holds the value of id in our talbe 'user'.
 
 $stmt -> execute(); //executes the query
 
-$stmt -> store_result(); //stores it
+$stmt -> store_result();
 
-$stmt -> bind_result($id, $name, $email); //binds the result, all the columns in our table should be included inside the parameter
+$stmt -> bind_result($id, $name, $email);//binds the result, all the columns in our talbe should be included inside the parameter
 
-$stmt -> fetch(); //then we take the data so we can echo it.
+$stmt -> fetch(); //then we take the data so we can echo it 
 
 echo $name."'s email is ".$email;
-
-
+//this should echo the data which has the id of 2
 ?>
